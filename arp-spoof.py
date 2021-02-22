598f8441e7f8952f672c109615166e78184bf88f
Evgeny Kuryatov <evgeny.kuryatov@netcracker.com>
2021-02-22 14:24:51 +0300

    Update script

--- arp-spoof.py
+++ arp-spoof.py
@@ -1,16 +1,17 @@
 #!venv/bin/python
 
-import scapy
+import scapy.all as scapy
+from pprint import pprint
 
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
-    return answered_list[0][1].hwsrc
+    answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)
+    return answered_list
 
 
 if __name__ == "__main__":
     mac = get_mac("192.168.56.102")
-    print(mac)
+    print(pprint(mac))

